f277cc5d4ef38f7d8ef8ce6c079409ee328d8afa
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-04 01:23:34 +0100

    aggiunti console.log

--- myscript.js
+++ myscript.js
@@ -1,7 +1,9 @@
 let yourName = prompt("Insert Name");
+console.log(yourName)
 
 let yourSurname = prompt("Insert Surname");
+console.log(yourSurname)
 
 let favouriteColor = prompt("Insert favourite Color");
-
+console.log(favouriteColor)
 
