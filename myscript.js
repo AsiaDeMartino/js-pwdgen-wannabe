9494ebfe09a1aabdd241da8b2f26da48b982c053
asia_demartino@libero.it <asia_demartino@libero.it>
2022-02-04 02:10:48 +0100

    aggiunta variabile passwod e testo html

--- myscript.js
+++ myscript.js
@@ -7,3 +7,14 @@ console.log(yourSurname)
 let favouriteColor = prompt("Insert favourite Color");
 console.log(favouriteColor)
 
+let yourPassword = (yourName) + (yourSurname) + (favouriteColor) + "21";
+console.log(yourPassword)
+
+
+let htmlPassword = document.getElementById("password").innerHTML
+
+document.getElementById("password").innerHTML = htmlPassword + yourPassword
+
+
+// document.getElementById("password").innerHTML += yourPassword
+
